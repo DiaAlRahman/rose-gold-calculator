244edb14b0b59944789da2e79bbea04030cc0d05
DiaAlRahman <127028600+DiaAlRahman@users.noreply.github.com>
2024-01-30 22:24:10 +0600

    Add digital font Add basic stucture Add core js functions

---
+++ script.js
@@ -0,0 +1,43 @@
+// These functions are the core responsiblities of a calculator, they will take two numbers
+// and return an answer according to that.
+function add(prev, next) {
+    return prev + next;
+};
+function subtract(prev, next) {
+    return prev - next;
+};
+function multiply(prev, next) {
+    return prev * next;
+};
+function divide(prev, next) {
+    return prev / next;
+};
+
+// These are the variables that will be used to interact with the display later.
+let n1, n2 = 0;
+let operator = "";
+
+function operate(prev, operator, next) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(prev, next);
+            console.log(result);
+            break;
+        case "-":
+            result = subtract(prev, next);
+            console.log(result);
+            break;
+        case "*":
+            result = multiply(prev, next);
+            console.log(result);
+            break;
+        case "/":
+            result = divide(prev, next);
+            console.log(result);
+            break;
+    };
+};
+
+operate(1, "+", 2);
+
